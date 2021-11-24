892430ecc24ecf2f35370768928f9a37df70bddb
Panayotis Manganaris <panos.manganaris@gmail.com>
2021-11-24 16:35:04 -0500

    added simple plot example to tests

--- tests/test_spyglass.py
+++ tests/test_spyglass.py
@@ -1,17 +1,20 @@
 from spyglass import __version__
-
+import spyglass
+import spyglass
+import numpy as np
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def test_annotfig():
+    eda = spyglass.EDA.EDAFigure
+
     xy = np.random.random((10,2))
     labels = ["Label for instance #{0}".format(i) for i in np.arange(xy.shape[0])]
-    fig, ax = plt.subplots()
 
-    scfig = testplot(xy[:, 0], xy[:, 1], labels, ax=ax)
-
-    plt.show()
+    fig, ax = plt.subplots(1,1, FigureClass=eda, subplot_kw={'projection':'interactive'})
+    fig.add_button()
+    ax.activescatter(xy[:,0], xy[:,1], labels)
 
     assert True
     
