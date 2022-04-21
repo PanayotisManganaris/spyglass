dfe7a0577ab0af2c1f84e84fbd292b9159fe86b3
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-04-21 17:25:51 -0400

    package transient but usable

--- spyglass/__init__.py
+++ spyglass/__init__.py
@@ -1,51 +1,51 @@
 __version__ = '0.1.2'
 
 #consider moving all of this to a dedicated Backend subpackage? see hvplot package?
-from spyglass.spyglass.plotmodule import (
-    PlotModule,
-    GraphModule,
-)
-# for full api compliance, a selection of modules should be supplied, including:
-#    deregister,
-#    register,
+# from spyglass.spyglass.plotmodule import (
+#     PlotModule,
+#     GraphModule,
+# )
+# # for full api compliance, a selection of modules should be supplied, including:
+# #    deregister,
+# #    register,
 
-#for linting? static type checkers?
-if TYPE_CHECKING:
-    from pandas.plotting._matplotlib.core import MPLPlot
+# #for linting? static type checkers?
+# if TYPE_CHECKING:
+#     from pandas.plotting._matplotlib.core import MPLPlot
 
-PLOT_CLASSES: dict[str, type[MPLPlot]] = {
-    "line": LinePlot,
-    "bar": BarPlot,
-    "barh": BarhPlot,
-    "box": BoxPlot,
-    "hist": HistPlot,
-    "kde": KdePlot,
-    "area": AreaPlot,
-    "pie": PiePlot,
-    "scatter": ScatterPlot,
-    "hexbin": HexBinPlot,
-}
+# PLOT_CLASSES: dict[str, type[MPLPlot]] = {
+#     "line": LinePlot,
+#     "bar": BarPlot,
+#     "barh": BarhPlot,
+#     "box": BoxPlot,
+#     "hist": HistPlot,
+#     "kde": KdePlot,
+#     "area": AreaPlot,
+#     "pie": PiePlot,
+#     "scatter": ScatterPlot,
+#     "hexbin": HexBinPlot,
+# }
 
-#public plot method provides spyglass as pandas plotting backend
-def plot(data, kind, **kwargs):
-    # Importing pyplot at the top of the file (before the converters are
-    # registered) causes problems in matplotlib 2 (converters seem to not
-    # work)
-    import matplotlib.pyplot as plt
+# #public plot method provides spyglass as pandas plotting backend
+# def plot(data, kind, **kwargs):
+#     # Importing pyplot at the top of the file (before the converters are
+#     # registered) causes problems in matplotlib 2 (converters seem to not
+#     # work)
+#     import matplotlib.pyplot as plt
 
-    if kwargs.pop("reuse_plot", False):
-        ax = kwargs.get("ax")
-        if ax is None and len(plt.get_fignums()) > 0:
-            with plt.rc_context():
-                ax = plt.gca()
-            kwargs["ax"] = getattr(ax, "left_ax", ax)
-    plot_obj = PLOT_CLASSES[kind](data, **kwargs)
-    plot_obj.generate()
-    plot_obj.draw()
-    return plot_obj.result
+#     if kwargs.pop("reuse_plot", False):
+#         ax = kwargs.get("ax")
+#         if ax is None and len(plt.get_fignums()) > 0:
+#             with plt.rc_context():
+#                 ax = plt.gca()
+#             kwargs["ax"] = getattr(ax, "left_ax", ax)
+#     plot_obj = PLOT_CLASSES[kind](data, **kwargs)
+#     plot_obj.generate()
+#     plot_obj.draw()
+#     return plot_obj.result
 
-__all__ = [
-    "plot", #what's this? the accessor?, the public method?
-    "PlotModule",
-    "GraphModule", #? or keys?
-]
+# __all__ = [
+#     "plot", #what's this? the accessor?, the public method?
+#     "PlotModule",
+#     "GraphModule", #? or keys?
+# ]
